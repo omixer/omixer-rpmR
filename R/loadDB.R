254a81f63fe4832a6dc94aa0f5dcd6095163b4ab
omixer <youssef.darzi@gmail.com>
2019-02-12 15:30:26 +0900

    Simplification and optimization of code

--- R/loadDB.R
+++ R/loadDB.R
@@ -1,6 +1,12 @@
 loadDB <- function(name) {
 	db.path <- system.file("extdata", package = "omixerRpm")
 	db <- ModuleDB(directory = db.path, modules = paste0(name, ".txt"), module.names.file=paste0(name, ".names"))
+	db@module.names <- read.table(
+			file.path(db@directory, db@module.names.file),
+			row.names=1,
+			sep="\t",
+			header=F, 
+			as.is=T)
 	print(paste("Loaded", name))
 	db
 }
--- R/loadDefaultDB.R
+++ R/loadDefaultDB.R
@@ -1,6 +1,3 @@
 loadDefaultDB <- function() {
-	gmms.db.path <- system.file("extdata", package = "omixerRpm")
-	gmms.db <- ModuleDB(directory = gmms.db.path, modules = "GMMs.v1.07.txt", module.names.file="GMMs.v1.07.names")
-	print("Loaded GMMs v1.07")
-	gmms.db
+	loadDB("GMMs.v1.07")
 }
